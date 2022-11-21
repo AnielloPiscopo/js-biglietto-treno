21759124945414486cc024f0accb5ca8261d1d46
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-21 15:35:31 +0100

    Removed the bootstrap links in the head of the html and added the first lines of work in the js file

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,11 @@ INIZIALIZZAZIONE E DICHIARAZIONI DI VARIABILI E COSTANTI */
 MAIN CODE */
 
 // * LINEAR PROGRAMM
+
+// ? INITIAL ALERTS
+alert('Benvenuto!\nQuesto programma permette di fornire il prezzo del biglietto del treno che dovrai pagare in base a quanti km percorrerai.');
+alert('Il prezzo viene calcolato secondo queste regole:\n1)Ogni km vale 0.21$;\n2)Viene applicato uno sconto del 20% per i minorenni;\n3)Viene applicato uno sconto del 40% per gli over 65');
+alert('Detto questo');
+
+
+// ? DATI FORNITI DALL'UTENTE TRAMITE PROMPT
